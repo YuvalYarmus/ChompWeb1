795ea026aa0efea90712741a007256af0ec44412
YuvalYarmus <yarmusyuval@gmail.com>
2021-01-28 03:30:45 +0200

    added some more styles. now trying to imrove the drawing on the canvas.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,26 @@
 const colors = require('tailwindcss/colors')
+const { fontFamily } = require('tailwindcss/defaultTheme')
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: [],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    // fontFamily: {
+    //   customBell: ['Bellefair'],
+    //   customAll: [`AllagiaDemo`],
+    //   customRoboto: ['Roboto'],
+    //   sans: ['Bellefair', 'AllagiaDemo', ...defaultTheme.fontFamily.sans],
+    //   serif: ['Bellefair', 'AllagiaDemo', ...defaultTheme.fontFamily.serif],
+    //   mono: [...defaultTheme.fontFamily.mono]
+    // },
     extend: {
       fontFamily: {
-        sans: ['Bellefair', 'AllagiaDemo', ...defaultTheme.fontFamily.sans],
-        serif: ['Bellefair','AllagiaDemo', ...defaultTheme.fontFamily.serif],
+        sans: [...defaultTheme.fontFamily.sans],// sans: ['Bellefair', 'AllagiaDemo', ...defaultTheme.fontFamily.sans],
+        serif: [...defaultTheme.fontFamily.serif],// serif: ['Bellefair', 'AllagiaDemo', ...defaultTheme.fontFamily.serif],
+        mono: [...defaultTheme.fontFamily.mono],
+        customBell: ['Bellefair'],
+        customRoboto: ['Roboto'],
       },
       colors: {
         grayWhite: '#dbdbdb',
